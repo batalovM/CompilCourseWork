28533c5cec98397e222b53df0e6fbe87f4088c87
Batalov Maksim <117968674+batalovM@users.noreply.github.com>
2025-03-16 14:17:13 +0700

    Update Parser.cs

--- CompilCourseWork/model/Parser.cs
+++ CompilCourseWork/model/Parser.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 class Parser
@@ -13,29 +13,35 @@ class Parser
 
     public void Parse()
     {
-        if (ParseConstDeclaration())
+        if (ParseConstDeclaration() && position == tokens.Count)
         {
             Console.WriteLine("Синтаксический анализ завершен успешно!");
         }
         else
         {
-            Console.WriteLine("Синтаксическая ошибка в позиции: " + tokens[position].Position);
+            Console.WriteLine("Синтаксическая ошибка в позиции: " + (position < tokens.Count ? tokens[position].Position : "конец входных данных"));
         }
     }
 
     private bool ParseConstDeclaration()
     {
-        return Match(14) && Match(14) && Match(2) && Match(15) && Match(14) && Match(10) && ParseExpression() && Match(16);
+        return Match(14) && Match(14) && Match(2) && Match(15) && (Match(20) || Match(21) || Match(22)) && Match(10) && ParseExpression() && Match(16);
     }
 
     private bool ParseExpression()
     {
-        return Match(1) || Match(3);
+        return Match(3); // Только вещественное число
     }
 
     private bool Match(int expectedCode)
     {
-        if (position < tokens.Count && tokens[position].Code == expectedCode)
+        if (position >= tokens.Count)
+        {
+            Console.WriteLine($"Ошибка: достигнут конец входных данных, ожидался токен с кодом {expectedCode}");
+            return false;
+        }
+
+        if (tokens[position].Code == expectedCode)
         {
             position++;
             return true;
