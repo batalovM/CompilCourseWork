4aeaeb4e2aafe40d4a3f29f324e4b248e9aa3c9f
Batalov Maksim <117968674+batalovM@users.noreply.github.com>
2025-04-09 12:37:16 +0700

    Update Parser.cs

--- CompilCourseWork/model/Parser.cs
+++ CompilCourseWork/model/Parser.cs
@@ -1,246 +1,247 @@
-using System;
-using System.Collections.Generic;
-
-public class Parser
+class Parser
 {
-    private readonly string input;
-    private int position;
-    public readonly List<ParseError> errors = new();
-    private bool hasCriticalError;
-
-    public Parser(string input)
+    static int _pos;  
+    static bool ParseKeyword(string _input, string expectedKeyword)
     {
-        this.input = input.Trim();
-        position = 0;
-    }
-
-    public void Parse()
-    {
-        ParseConst();
-        if (!hasCriticalError) ParseVal();
-        if (!hasCriticalError) ParseIdentifier();
-        if (!hasCriticalError) ParseType();
-        if (!hasCriticalError) ParseValue();
-        
-        CheckFinalSemicolon();
-
-        PrintErrors();
-    }
-    private void CheckFinalSemicolon()
-    {
-        int lastCharPos = input.Length - 1;
-        while (lastCharPos >= 0 && char.IsWhiteSpace(input[lastCharPos]))
+        bool success = true;
+        List<char> trashChars = new List<char>();
+        int initialPos = _pos; 
+        while (_pos < _input.Length && _input[_pos] != expectedKeyword[0])
         {
-            lastCharPos--;
-        }
-
-        if (lastCharPos < 0 || input[lastCharPos] != ';')
-        {
-            AddError(lastCharPos >= 0 ? lastCharPos + 1 : 0, 
-                "Ожидается завершающий символ ';'");
-        }
-    }
-
-    private void ParseConst()
-    {
-        var startPos = position;
-        if (!ExpectKeyword("const", ref startPos))
-        {
-            if (LookAhead("val"))
-                AddError(0, "Ожидается ключевое слово 'const'");
-            else
-                AddError(startPos, "Ожидается 'const'");
-            hasCriticalError = true;
-            return;
-        }
-
-        position = startPos;
-        if (!CheckSpaceAfter())
-        {
-            AddError(position, "Ожидается пробел после 'const'");
-        }
-    }
-
-    private void ParseVal()
-    {
-        var startPos = position;
-        if (!ExpectKeyword("val", ref startPos))
-        {
-            AddError(startPos, "Ожидается 'val'");
-            hasCriticalError = true;
-            return;
-        }
-
-        position = startPos;
-        if (!CheckSpaceAfter())
-        {
-            AddError(position, "Ожидается пробел после 'val'");
-        }
-    }
-
-    private void ParseIdentifier()
-    {
-        SkipWhitespace();
-        var startPos = position;
-
-        if (position >= input.Length || !char.IsLetter(input[position]))
-        {
-            AddError(startPos, "Идентификатор должен начинаться с буквы");
-            hasCriticalError = true;
-            return;
-        }
-
-        while (position < input.Length && (char.IsLetterOrDigit(input[position]) || input[position] == '_'))
-        {
-            position++;
-        }
-
-        // Проверка что идентификатор не пустой
-        if (position == startPos)
-        {
-            AddError(startPos, "Отсутствует идентификатор");
-            hasCriticalError = true;
-        }
-    }
-
-    private void ParseType()
-    {
-        SkipWhitespace();
-        var startPos = position;
-
-        if (position >= input.Length || input[position] != ':')
-        {
-            AddError(startPos, "Ожидается символ ':'");
-            hasCriticalError = true;
-            return;
-        }
-        position++;
-
-        SkipWhitespace();
-        startPos = position;
-
-        if (!ExpectKeyword("Double", ref position))
-        {
-            AddError(startPos, "Ожидается тип 'Double'");
-            hasCriticalError = true;
-        }
-    }
-
-    private void ParseValue()
-    {
-        SkipWhitespace();
-        var startPos = position;
-
-        if (position >= input.Length || input[position] != '=')
-        {
-            AddError(startPos, "Ожидается символ '='");
-            hasCriticalError = true;
-            return;
-        }
-        position++;
-
-        SkipWhitespace();
-        startPos = position;
-
-        if (!ParseNumber())
-        {
-            AddError(startPos, "Ожидается числовое значение");
-            hasCriticalError = true;
-        }
-    }
-
-    private bool ParseNumber()
-    {
-        bool hasDigits = false;
-
-        // Опциональный знак
-        if (position < input.Length && (input[position] == '+' || input[position] == '-'))
-        {
-            position++;
-        }
-
-        // Целая часть
-        while (position < input.Length && char.IsDigit(input[position]))
-        {
-            position++;
-            hasDigits = true;
-        }
-
-        // Дробная часть
-        if (position < input.Length && input[position] == '.')
-        {
-            position++;
-            while (position < input.Length && char.IsDigit(input[position]))
+            if (_input[_pos] == 'v' || _input[_pos] == 'D' || _input[_pos] == '=')
             {
-                position++;
-                hasDigits = true;
+                Console.WriteLine($"Ожидалость ключевое слово {expectedKeyword}");
+                return false;
+            }
+            trashChars.Add(_input[_pos]);
+            _pos++;
+        }
+
+        if (trashChars.Count != 0)
+        {
+            Console.WriteLine($"Неизвестная конструкция в позиции {initialPos}: отброшенный фрагмент: {new string(trashChars.ToArray())}");
+        }
+        
+        int wordStartPos = _pos;
+        int expectedIndex = 0;
+        bool alignmentDone = false;
+
+        while (_pos < _input.Length && expectedIndex < expectedKeyword.Length)
+        {
+            char inputChar = _input[_pos];
+            char expectedChar = expectedKeyword[expectedIndex];
+
+            if (inputChar == expectedChar)
+            {
+                expectedIndex++;
+                _pos++;
+            }
+            else
+            {
+                int lookaheadIndex = -1;
+                if (expectedIndex + 1 < expectedKeyword.Length)
+                {
+                    lookaheadIndex = expectedKeyword.IndexOf(inputChar, expectedIndex + 1);
+                }
+                if (lookaheadIndex != -1)
+                {
+                    Console.WriteLine($"Ожидалось ключевое слово:'{expectedKeyword}'");
+                    expectedIndex = lookaheadIndex;
+                    alignmentDone = true; 
+                    if (inputChar == expectedKeyword[expectedIndex])
+                    {
+                        expectedIndex++;
+                        _pos++;
+                    } else {
+                         success = false; 
+                         _pos++; 
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка: Неожиданный символ '{inputChar}' на позиции {_pos}");
+                    success = false;
+                    _pos++; 
+                }
             }
         }
+        if (expectedIndex < expectedKeyword.Length)
+        {
+            if (success && !alignmentDone)
+            {
+                 Console.WriteLine($"Ошибка: Ожидалось '{expectedKeyword}', но строка закончилась (проверка началась с {wordStartPos})");
+            } else if (!success) {
+                 Console.WriteLine($"Ошибка: Ключевое слово '{expectedKeyword}' не распознано полностью из-за ошибок (проверка началась с {wordStartPos})");
+            } else if (alignmentDone) {
+                 Console.WriteLine($"Ошибка: Ключевое слово '{expectedKeyword}' не завершено после выравнивания (проверка началась с {wordStartPos})");
+            }
 
-        return hasDigits;
+            success = false;
+        }
+
+        return success;
     }
-    
-
-    private bool ExpectKeyword(string keyword, ref int pos)
+    static bool ParseId(string _input)
     {
-        SkipWhitespace(ref pos);
-        if (pos + keyword.Length > input.Length) return false;
-
-        bool match = input.Substring(pos, keyword.Length).Equals(keyword);
-        if (match) pos += keyword.Length;
-        return match;
-    }
-
-    private bool CheckSpaceAfter()
-    {
-        if (position < input.Length && !char.IsWhiteSpace(input[position])) 
-            return false;
+        bool success = true;
+        List<char> trashChars = new List<char>();
         
-        SkipWhitespace();
-        return true;
-    }
-
-    private void SkipWhitespace()
-    {
-        while (position < input.Length && char.IsWhiteSpace(input[position]))
+        while (_pos < _input.Length && !(char.IsLetter(_input[_pos]) && _input[_pos] != '_'))
         {
-            position++;
-        }
-    }
-
-    private void SkipWhitespace(ref int pos)
-    {
-        while (pos < input.Length && char.IsWhiteSpace(input[pos]))
-        {
-            pos++;
-        }
-    }
-
-    private bool LookAhead(string keyword)
-    {
-        return position + keyword.Length <= input.Length && 
-               input.Substring(position, keyword.Length).Equals(keyword);
-    }
-
-    private void AddError(int errorPosition, string message)
-    {
-        errors.Add(new ParseError(errorPosition, message));
-    }
-
-    public void PrintErrors()
-    {
-        if (errors.Count == 0)
-        {
-            Console.WriteLine("Разбор завершен успешно!");
-            return;
+            if (_input[_pos] == ':')
+            {
+                Console.WriteLine($"Ожидалась переменная");
+                return false;
+            }
+            trashChars.Add(_input[_pos]);
+            _pos++;
         }
 
-        Console.WriteLine($"Найдено {errors.Count} ошибок:");
-        foreach (var error in errors)
+        if (trashChars.Count != 0)
         {
-            Console.WriteLine($"Позиция {error.Position}: {error.Message}");
+            Console.WriteLine($"Неизвестная конструкция в позиции {_pos - trashChars.Count}: отброшенный фрагмент: {new string(trashChars.ToArray())}");
         }
-    }
+        
+        if (_pos < _input.Length && (char.IsLetter(_input[_pos]) || _input[_pos] == '_'))
+        {
+            while (_pos < _input.Length && (char.IsLetterOrDigit(_input[_pos]) || _input[_pos] == '_' || char.IsDigit(_input[_pos])))
+            {
+                _pos++;
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Ошибка: ожидался идентификатор (начинается с буквы или '_') на позиции {_pos}");
+            success = false;
+        }
 
-    public record ParseError(int Position, string Message);
+        return success;
+    }
+    static bool ParseColon(string _input)
+    {
+        bool success = true;
+
+        if (_pos < _input.Length && _input[_pos] == ':')
+        {
+            _pos++;
+        }
+        else
+        {
+            Console.WriteLine($"Ошибка: ожидалось двоеточие ':' на позиции {_pos}");
+            success = false;
+        }
+
+        return success;
+    }
+    static bool ParseEqual(string _input)
+    {
+        bool success = true;
+
+        if (_pos < _input.Length && _input[_pos] == '=')
+        {
+            _pos++;
+        }
+        else
+        {
+            Console.WriteLine($"Ошибка: ожидалось знак равенства '=' на позиции {_pos}");
+            success = false;
+        }
+
+        return success;
+    }
+    static bool ParseNum(string _input)
+    {
+        bool success = true;
+        List<char> trashChars = new List<char>();
+        
+        while (_pos < _input.Length && !char.IsDigit(_input[_pos]))
+        {
+            trashChars.Add(_input[_pos]);
+            _pos++;
+        }
+
+        if (trashChars.Count != 0)
+        {
+            Console.WriteLine($"Неизвестная конструкция в позиции {_pos - trashChars.Count}: отброшенный фрагмент: {new string(trashChars.ToArray())}");
+        }
+        
+        if (_pos < _input.Length && char.IsDigit(_input[_pos]))
+        {
+            while (_pos < _input.Length && char.IsDigit(_input[_pos]))
+            {
+                _pos++;
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Ошибка: ожидалось число на позиции {_pos}");
+            success = false;
+        }
+
+        return success;
+    }
+    static bool ParseEnd(string _input)
+    {
+        bool success = true;
+
+        if (_pos < _input.Length && _input[_pos] == ';')
+        {
+            _pos++;
+        }
+        else
+        {
+            Console.WriteLine($"Ошибка: ожидался завершающий символ ';' на позиции {_pos}");
+            success = false;
+        }
+
+        return success;
+    }
+    static void Parse(string _input)
+    {
+        if (ParseKeyword(_input, "const"))
+        {
+            if (_input[_pos] != ' ')
+            {
+                Console.WriteLine($"Ошибка: ожидался пробел после 'const''");
+            }
+        }
+        while (_pos < _input.Length && char.IsWhiteSpace(_input[_pos]))
+        {
+            _pos++;
+        }
+        
+        if (ParseKeyword(_input, "val"))
+        {
+            if (_input[_pos] != ' ')
+            {
+                Console.WriteLine($"Ошибка: ожидался пробел после 'val''");
+            }
+        }
+        
+        while (_pos < _input.Length && char.IsWhiteSpace(_input[_pos]))
+        {
+            _pos++;
+        }
+        ParseId(_input);
+        while (_pos < _input.Length && char.IsWhiteSpace(_input[_pos]))
+        {
+            _pos++;
+        }
+        ParseColon(_input);
+        ParseKeyword(_input, "Double");
+        ParseEqual(_input);
+        while (_pos < _input.Length && char.IsWhiteSpace(_input[_pos]))
+        {
+            _pos++;
+        }
+        ParseNum(_input);
+        ParseEnd(_input);
+    
+    }
+    static void Main(string[] args)
+    {
+        Parse("const val pi:Double=123;");
+    }
+    //TODO ошибка на отсутствие ключевого слова
 }
+
